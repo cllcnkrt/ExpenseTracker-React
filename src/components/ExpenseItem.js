55a038bf960f12a076acf24409ed73f3e7da2a72
cllcnkrt <cllcnkrt@outlook.com>
2021-06-06 21:57:53 +0300

    Project created.

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,19 @@
+import react from "react";
+import "./ExpenseItem.css";
+
+
+
+
+function ExpenseItem(props) {
+
+const expenseDate = new Date(2021, 2, 28);
+
+
+  return (
+   
+  );
+}
+
+export default ExpenseItem;
+
+
